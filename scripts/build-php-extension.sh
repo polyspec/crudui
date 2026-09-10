93a3cbce10d339f915f2ac5b5e7577ba3aec1fc9
min-median-max <max@blue.tools>
2026-09-10 16:48:22 +0900

    Preserve PHP extension test sources during builds

--- scripts/build-php-extension.sh
+++ scripts/build-php-extension.sh
@@ -3,7 +3,14 @@ set -eu
 
 project_root=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 cd "$project_root/packages/php-ext"
-phpize --clean
+rm -rf \
+  .libs autom4te.cache build include modules native/.libs target/release/build \
+  Makefile Makefile.fragments Makefile.objects \
+  config.cache config.h config.h.in config.h.in~ config.log config.nice \
+  config.status configure configure.ac configure~ confdefs.h libtool \
+  crudui.la run-tests.php \
+  native/errors.dep native/errors.lo native/crudui.dep native/crudui.lo \
+  native/values.dep native/values.lo
 phpize
 ./configure --enable-crudui
 make -j"${CRUDUI_BUILD_JOBS:-2}"
